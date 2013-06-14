da329f4f7437a5d562735493abaa92434970bf20
Ralf Westphal <ralfw08@gmail.com>
2013-06-14 04:40:57 -0500

    Willkommensseite rausgenommen. App startet sofort mit Zwischenrufformular.

--- Heckle Service Mobile Client/js/membran/view.js
+++ Heckle Service Mobile Client/js/membran/view.js
@@ -1,13 +1,6 @@
 function View() {
     var self = this;
-    
-    // --- Event Handlers ---
-     $('#pageZwischenrufen').live('pagebeforeshow', function() {
-        $("#txtNachricht")[0].value = "";
-        
-        $('#paneKomponieren')[0].style.display = 'block';
-        $('#paneQuittung')[0].style.display = 'none';
-    });
+
     
     $("#btnZwischenrufen")[0].onclick = function() {
         var nachrichtenteile = {
@@ -16,6 +9,13 @@ function View() {
             email: $('#txtEmail')[0].value
         }
         self.Zwischenruf(nachrichtenteile);
+    }
+    
+    $("#btnNochmal")[0].onclick = function() {
+        $("#txtNachricht")[0].value = "";
+        
+        $('#paneKomponieren')[0].style.display = 'block';
+        $('#paneQuittung')[0].style.display = 'none';
     }
     
     
