2cb5fb03eb355ef865c94659e60ae19aae5913b2
Ralf Westphal <ralfw08@gmail.com>
2013-06-14 09:13:48 -0500

    Startup code vereinfacht

---
+++ Heckle Service Mobile Client/js/main.js
@@ -0,0 +1,9 @@
+// Wait for PhoneGap to load
+document.addEventListener(
+    "deviceready", 
+    function() {
+        navigator.splashscreen.hide();
+        Integrieren();
+    }, 
+    false);
+
