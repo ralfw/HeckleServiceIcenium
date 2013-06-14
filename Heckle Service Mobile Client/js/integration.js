dca0145f94e5aa4ccf9084035828eb170123c1cf
Ralf Westphal <ralfw08@gmail.com>
2013-06-14 12:49:27 -0500

    Bauen und binden in eigene Methode in Main verschoben.

--- Heckle Service Mobile Client/js/integration.js
+++ Heckle Service Mobile Client/js/integration.js
@@ -1,12 +1,3 @@
-function Integrieren() {
-    var presenter = new Presenter();
-    presenter.Zwischenruf = function(nachrichtenteile) {
-        Zwischenrufen(nachrichtenteile, 
-                      presenter.Nachricht_anzeigen, 
-                      presenter.Fehler_anzeigen);
-    }
-}
-
 function Zwischenrufen(nachrichtenteile, bei_Erfolg, bei_Misserfolg) {
     var nachricht = Nachricht_formulieren(nachrichtenteile);
     Versenden(nachricht, bei_Erfolg, bei_Misserfolg);
--- Heckle Service Mobile Client/js/main.js
+++ Heckle Service Mobile Client/js/main.js
@@ -1,9 +1,19 @@
+function Bauen_und_binden() {
+    var presenter = new Presenter();
+    
+    presenter.Zwischenruf = function(nachrichtenteile) {
+        Zwischenrufen(nachrichtenteile, 
+                      presenter.Nachricht_anzeigen, 
+                      presenter.Fehler_anzeigen);
+    }
+}
+
 // Wait for PhoneGap to load
 document.addEventListener(
     "deviceready", 
     function() {
         navigator.splashscreen.hide();
-        Integrieren();
+        Bauen_und_binden();
     }, 
     false);
 
